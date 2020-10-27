84b2d1550dd152a317719f6ad5834e9343b99567
JaDee <324810875@qq.com>
2020-10-27 10:22:34 +0800

    fix for gif support
    修复可能不是以.gif结尾的动态图导致不能支持的bug

--- SKPhotoBrowser/SKPhoto.swift
+++ SKPhotoBrowser/SKPhoto.swift
@@ -83,7 +83,7 @@ open class SKPhoto: NSObject, SKPhotoProtocol {
                     return
                 }
 
-                if let data = data, let response = response, let image = self.photoURL.lowercased().hasSuffix(".gif") == true ? UIImage.gif(data: data) : UIImage(data: data) {
+                if let data = data, let response = response, let image = self.photoURL.lowercased().contains(".gif") == true ? UIImage.gif(data: data) : UIImage(data: data) {
                     if self.shouldCachePhotoURLImage {
                         if SKCache.sharedCache.imageCache is SKRequestResponseCacheable {
                             SKCache.sharedCache.setImageData(data, response: response, request: task?.originalRequest)
